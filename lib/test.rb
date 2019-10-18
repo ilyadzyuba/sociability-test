8095290a32410cf7da4fe63dc836b40f876b49bb
Ilya Dzyuba <0147431@gmail.com>
2019-10-18 18:21:43 +0300

    methods separation

--- lib/test.rb
+++ lib/test.rb
@@ -1,28 +1,39 @@
 class Test
-  def initialize(questions)
-    @questions = questions
-    @score = 0 # учет ответов
+  def initialize(q_hash)
+    @q_hash = q_hash
+    @score = 0 # подсчет ответов
+    @choices = [] # ответы
+    @count = 1
   end
 
-  def score
-    @questions.each do |question| # Выводим вопрос, предлагаем ответы, считаем ответы.
-      puts "\n#{question}"
+  def query # Задаем вопрос
+    @print_query = @q_hash.fetch(@count)
+    @count += 1
+    @print_query
+  end
+
+  def answers # Собираем ответы
+    puts "1. Да"
+    puts "2. Нет"
+    puts "3. Иногда"
+    choice = STDIN.gets.to_i
+    while choice != 1 && choice != 2 && choice != 3 do # существующий ответ?
+      puts "\nВведите существующий вариант"
       puts "1. Да"
       puts "2. Нет"
-      puts "3. Иногда\n\n"
+      puts "3. Иногда"
       choice = STDIN.gets.to_i
-      while choice != 1 && choice != 2 && choice != 3 do # Проверяем, что выбран существующий ответ
-        puts "\nВведите существующий вариант"
-        puts "1. Да"
-        puts "2. Нет"
-        puts "3. Иногда\n\n"
-        choice = STDIN.gets.to_i
-      end
-      if choice == 1 # если ответ да - 2 балла
+    end
+    @choices << choice
+  end
+
+  def score # Пересчитываем ответы в баллы теста
+    @choices.each do |answer|
+      if answer == 1 # если ответ "да" - 2 балла
         @score += 2
-      elsif choice == 3 # если ответ иногда - 1 балл
+      elsif answer == 3 # если ответ "иногда" - 1 балл
         @score += 1
-      end # если ответ нет 0 баллов
+      end # если ответ "нет" 0 баллов
     end
     @score
   end
--- main.rb
+++ main.rb
@@ -1,6 +1,8 @@
 require_relative "lib/test"
 require_relative "lib/result"
 
+count = 1
+
 results_path = __dir__ + "/data/results.txt"
 results_file = File.new(results_path, "r:UTF-8")
 unless File.exist?(results_path)
@@ -15,12 +17,26 @@ unless File.exist?(questions_path)
   abort 'Файл с вопросами не найден!'
 end
 questions = questions_file.readlines
+# Делаем hash из вопросов для вывода
+q_hash = questions.each_with_object({}) do
+  |k, h| h[count] = k
+  count += 1
+end
 questions_file.close
 
 puts "Тест поможет определить ваш уровень коммуникабельности."
 puts "\nДля этого необходимо правдиво ответить на следующие вопросы.\n\n"
 
-test = Test.new(questions)
+# Количество вопросов
+num_q = q_hash.size
+
+test = Test.new(q_hash)
+while num_q > 0
+  puts "\n#{test.query}"
+  num_q -= 1
+  test.answers
+end
+
 result = Result.new(test.score, results)
 puts "\nРезультаты теста:\n\n"
-puts result.print
+puts "#{result.print}\n\n"
